47c1ba1b0f4734538f7b6b74d57f840c4947e0fa
Vinya H G <vinyagowda90@gmail.com>
2026-06-04 15:58:24 +0530

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,1 +1,19 @@
+import streamlit as st
 
+from utils.data_loader import load_data
+from utils.insights import get_kpis
+
+df = load_data()
+
+kpi = get_kpis(df)
+
+st.title("📊 Dashboard")
+
+c1,c2,c3,c4 = st.columns(4)
+
+c1.metric("Investors",kpi["Total Investors"])
+c2.metric("Avg Age",kpi["Average Age"])
+c3.metric("Male",kpi["Male Investors"])
+c4.metric("Female",kpi["Female Investors"])
+
+st.dataframe(df.head())
