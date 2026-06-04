bbebd40ac520368a23779c12702de8e90d4eb242
Vinya H G <vinyagowda90@gmail.com>
2026-06-04 15:57:21 +0530

    Update insights.py

--- utils/insights.py
+++ utils/insights.py
@@ -1,1 +1,8 @@
+def get_kpis(df):
 
+    return {
+        "Total Investors": len(df),
+        "Average Age": round(df["age"].mean(),1),
+        "Male Investors": len(df[df["gender"]=="Male"]),
+        "Female Investors": len(df[df["gender"]=="Female"])
+    }
