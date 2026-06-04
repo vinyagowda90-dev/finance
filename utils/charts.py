b7ae7b4e2319a2b0f06c20d77dc05d71081a55dd
Vinya H G <vinyagowda90@gmail.com>
2026-06-04 15:57:42 +0530

    Update charts.py

--- utils/charts.py
+++ utils/charts.py
@@ -1,1 +1,36 @@
+import plotly.express as px
 
+def gender_chart(df):
+
+    return px.pie(
+        df,
+        names="gender",
+        title="Gender Distribution"
+    )
+
+def age_chart(df):
+
+    return px.histogram(
+        df,
+        x="age",
+        nbins=10,
+        title="Age Distribution"
+    )
+
+def objective_chart(df):
+
+    return px.bar(
+        df["Objective"].value_counts().reset_index(),
+        x="Objective",
+        y="count",
+        title="Investment Objectives"
+    )
+
+def avenue_chart(df):
+
+    return px.bar(
+        df["Avenue"].value_counts().reset_index(),
+        x="Avenue",
+        y="count",
+        title="Preferred Investment Avenue"
+    )
