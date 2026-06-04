6ac1230e7c53c6cbfc6f683f598fd6278ee3fb98
Vinya H G <vinyagowda90@gmail.com>
2026-06-04 15:59:04 +0530

    Update Investment_Analysis.py

--- pages/Investment_Analysis.py
+++ pages/Investment_Analysis.py
@@ -1,1 +1,24 @@
+import streamlit as st
+import plotly.express as px
 
+from utils.data_loader import load_data
+
+df = load_data()
+
+st.title("📈 Investment Analysis")
+
+avenue = df["Avenue"].value_counts()
+
+fig = px.bar(
+    x=avenue.index,
+    y=avenue.values,
+    labels={
+        "x":"Investment Type",
+        "y":"Investors"
+    }
+)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
+)
