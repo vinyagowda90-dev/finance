914104191ba74b017395fb115c83448d40215b70
Vinya H G <vinyagowda90@gmail.com>
2026-06-04 15:59:25 +0530

    Update Risk_Return_Insights.py

--- pages/Risk_Return_Insights.py
+++ pages/Risk_Return_Insights.py
@@ -1,1 +1,29 @@
+import streamlit as st
+import plotly.express as px
 
+from utils.data_loader import load_data
+
+df = load_data()
+
+st.title("🎯 Return Expectations")
+
+fig = px.pie(
+    df,
+    names="Expect"
+)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
+)
+
+fig2 = px.bar(
+    df["Factor"].value_counts().reset_index(),
+    x="Factor",
+    y="count"
+)
+
+st.plotly_chart(
+    fig2,
+    use_container_width=True
+)
