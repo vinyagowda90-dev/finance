e6ce08df35da31b9895d26f2237b80fb013d3407
Vinya H G <vinyagowda90@gmail.com>
2026-06-04 15:58:45 +0530

    Update Investor_Profile.py

--- pages/Investor_Profile.py
+++ pages/Investor_Profile.py
@@ -1,1 +1,18 @@
+import streamlit as st
 
+from utils.data_loader import load_data
+from utils.charts import gender_chart,age_chart
+
+df = load_data()
+
+st.title("👥 Investor Profile")
+
+st.plotly_chart(
+    gender_chart(df),
+    use_container_width=True
+)
+
+st.plotly_chart(
+    age_chart(df),
+    use_container_width=True
+)
