51370b94cb589129e55adaf2299d32044e7999be
Vinya H G <vinyagowda90@gmail.com>
2026-06-04 15:59:46 +0530

    Update Advanced_Analytics.py

--- pages/Advanced_Analytics.py
+++ pages/Advanced_Analytics.py
@@ -1,1 +1,33 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+from utils.data_loader import load_data
+
+df = load_data()
+
+st.title("🔥 Advanced Analytics")
+
+numeric = [
+    "Mutual_Funds",
+    "Equity_Market",
+    "Debentures",
+    "Government_Bonds",
+    "Fixed_Deposits",
+    "PPF",
+    "Gold"
+]
+
+corr = df[numeric].corr()
+
+fig = px.imshow(
+    corr,
+    text_auto=True,
+    aspect="auto",
+    title="Investment Correlation Heatmap"
+)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
+)
